b221d64d3a778a64c46328e5ac47ab93f2987862
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2023-01-12 14:27:01 -0600

    ciclo de vida de un componente

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -11,8 +11,32 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
     imgDefault='https://andeguat.org.gt/wp-content/uploads/2015/02/default-placeholder.png';
     imgDefault2='https://www.shutterstock.com/image-vector/picture-vector-icon-no-image-600w-1732584341.jpg';
     //url avata= https://www.w3schools.com/howto/img_avatar.png
-    constructor(){}
+
+    //================ CICLO DE VIDA DE COMPONENTES ================================================
+    constructor(){
+      //before render
+      //not run async as -- just run one time
+      console.log('constructor','imgValue=>',this.imgDefault2)
+    }
+    ngOnchanges(){
+      //before and on during render
+      //su funcion es actualizar los inputs de nuestro componente, y se repite many times.
+      console.log('ngOnchanges','imgValue=>',this.imgDefault)
+    }
     ngOnInit(): void { 
+      //run before render
+      // in this block of code we can make de async functions or petitions to the backend or API'S, fetch, promise etc.
+      //this block run just one time
+      console.log('ngOnInit','imgValue=>',this.imgDefault)
+    }
+    ngAfterViewInit(){
+      //run after render 
+      //handler children
+      console.log('ngAfterViewInit')
+    }
+    ngOnDestroy(){
+      //delete
+      console.log('Ondestroy')
     }
     imgError(){
       this.img = this.imgDefault2;
